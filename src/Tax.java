e2cbf15c28380475ca67f1f55d1f22333cdd8849
SergeyPtica <SergeyPtica@users.noreply.github.com>
2017-03-19 18:29:16 +0200

    Add files via upload
    Second commit

---
+++ src/Tax.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Serg on 19.03.2017.
+ */
+class Tax {
+    double grossIncome; // class member variables
+    String state;
+    int dependents;
+    static int customerCounter;
+
+    //Constructor
+    Tax(double gi, String st, int depen){
+        grossIncome = gi;
+        state = st;
+        dependents = depen;
+        customerCounter++; // increment the counter by one
+        System.out.println("Preparing the tax data for customer #" + customerCounter);
+    }
+
+    public double calcTax(){
+        return (grossIncome*0.33 - dependents*100);
+    }
+
+    public static void convertToEuros(double taxInDollars){
+        System.out.println("Tax in Euros: " + taxInDollars/1.25);
+    }
+}
